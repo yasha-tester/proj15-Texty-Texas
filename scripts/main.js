00732b48d12c48dde7629023516a9a8985d6b7ff
yakovgavryushov <95687203+yakovgavryushov@users.noreply.github.com>
2022-07-07 21:01:18 +0300

    added a whole thing

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+function getOn(){
+    document.getElementById("titleTextH3").innerHTML = "So you on, brother!"
+}
+function resetFunc(){
+    document.getElementById("titleTextH3").style.visibility = "visible";
+    document.getElementById("titleTextH3").innerHTML = "Texty Texas";
+}
+function hide(){
+    document.getElementById("titleTextH3").style.visibility = "hidden";
+}
